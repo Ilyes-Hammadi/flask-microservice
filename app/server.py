50a93948a5a8ebe7b372dd34f704cb06dec3b519
Ilyes Hammadi <hammadiilyesahmed@gmail.com>
2017-10-16 17:47:30 +0200

    Initial Commit

---
+++ app/server.py
@@ -0,0 +1,12 @@
+#!flask/bin/python
+from flask import render_template
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return jsonify({'detail': 'hello world'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
